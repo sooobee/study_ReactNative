46030ca3753e4e0453ef513350df9736f2d3cf51
김수비 <soobee@soobee.local>
2023-07-23 18:48:53 +0200

    22 이벤트 처리하기

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import {useState} from 'react';
 import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
+  // enteredGoalText의 초기값을 '' 로 설정
+  // setEnteredGoalText를 이용해 값 변경 가능
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  
+  function goalInputHandler(enteredText) {
+    // enteredText를 enteredGoalText로 설정(setEnteredGoalText의 매개변수)
+    setEnteredGoalText(enteredText); 
+  }
+
+  function addGoalHandler() {
+    // enteredGoalText를 로그에 출력
+    console.log(enteredGoalText);
+  }
+
   return (
     <View style ={styles.appContainer}>
       <View style={styles.inputContainer}>
-          <TextInput style = {styles.TextInput} placeholder = "Your course goal!"/>
-          <Button title = "Add Goal"/> 
+          <TextInput style = {styles.TextInput} 
+          placeholder = "Your course goal!"
+          // 텍스트를 입력하면 함수 실행
+          onChangeText={goalInputHandler}
+          />
+          <Button 
+          title = "Add Goal"
+          // 버튼을 클릭하면 함수 실행
+          onPress={addGoalHandler}/> 
       </View>
 
       <View style = {styles.goalsContainer}>
@@ -21,7 +42,7 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     padding: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16za
   },
 
   // textInput과 button을 묶는 뷰
