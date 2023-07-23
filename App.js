47e4b9d594095a2c65e584e4dc45bcc55c7f269c
김수비 <soobee@soobee.local>
2023-07-23 17:43:30 +0200

    21 레이아웃 개선하기

--- App.js
+++ App.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
- 
+
 export default function App() {
   return (
     <View style ={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style = {styles.TextInput} placeholder = "Your course goal!"/>
-        <Button title = "Add Goal"/> 
-     </View>
-    <View>
-      <Text>List of goals...</Text>
+          <TextInput style = {styles.TextInput} placeholder = "Your course goal!"/>
+          <Button title = "Add Goal"/> 
+      </View>
+
+      <View style = {styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
-  </View>
   );  
 }
 
 const styles = StyleSheet.create({
+  // 전체 뷰
   appContainer: {
-    padding: 70,
+    flex: 1,
+    padding: 50,
+    paddingHorizontal: 16
   },
+
+  // textInput과 button을 묶는 뷰
   inputContainer: {
+    flex: 1, // 모든 공간 차지
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomColor: '#cccccc',
+    borderBottonWidth: 3,
   },
+
+  // 인풋 뷰
   TextInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
-    padding: 8}
-})
+    width: '70%',
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 5
+  }
+}) 
