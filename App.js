d6eca4ba9db01602c9714d002a9bf8c3ddda6389
김수비 <soobee@soobee.local>
2023-07-23 16:51:47 +0200

    20 플렉스 박스

--- App.js
+++ App.js
@@ -1,31 +1,39 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
  
 export default function App() {
-  return (
-    <View style ={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style = {styles.TextInput} placeholder = "Your course goal!"/>
-        <Button title = "Add Goal"/> 
-     </View>
-    <View>
-      <Text>List of goals...</Text>
+  return ( 
+    <View style ={{padding: 50, flexDirection: 'row'}}>
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+        >
+          <Text>1</Text>
+      </View>
+      <View
+        style = {{
+          backgroundColor: 'blue',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+        >
+        <Text>2</Text>
+      </View>
+      <View
+        style= {{
+          backgroundColor: 'green',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+        >
+          <Text>3</Text>
+      </View>
     </View>
-  </View>
-  );  
+  );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    padding: 70,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  TextInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
-    padding: 8}
-})
